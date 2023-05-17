c00a71740ff935bfc4620e2e7bbd3e4d44bf534f
Sean Sutherland <sean.sutherland@gotyto.com>
2023-05-17 16:19:58 -0700

    add Scrambles helper class

--- wordle-suggest.user.js
+++ wordle-suggest.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wordle Suggester
 // @namespace    https://srsutherland.dev
-// @version      2023.05.04
+// @version      2023.05.17
 // @description  Automatically generate a list of letter combinations that fit a provided pattern from the remaining wordle letters
 // @author       srsutherland
 // @match        https://www.nytimes.com/games/wordle/index.html
@@ -15,14 +15,7 @@
     const $ = (s) => document.querySelector(s)
     const $$ = (s) => Array.from(document.querySelectorAll(s))
 
-    const getLetters = () =>
-        Array.from(document.querySelectorAll(`.Key-module_key__kchQI`))
-            .filter(k => !k.ariaLabel?.match("absent"))
-            .map(k => k.innerText)
-            .filter(s => s.length == 1)
-
-    const getVowels = () => getLetters().filter(l => "AEIOUY".match(l))
-
+    // Letter pairs that don't appear in English words
     const impossiblePairs = [
         /b[fgnpqvx]/i, /c[bdfgjpqvx]/i, /d[cfkpqx]/i, /f[bcghkmnpqvwxz]/i, /g[bcfjkpqtvxz]/i,
         /h[bfghjkqvxz]/i, /j[bcfgklmpqstvxz]/i, /k[cdgjmpqxz]/i, /l[hqrx]/i, /m[cgjqwx]/i,
@@ -31,6 +24,71 @@
         /[hjklmqrwxz]{3}/i
     ]
     const isNotImpossible = (w) => !impossiblePairs.some(p => (w.match(p)))
+    
+    // Scrambles class for chaining filters
+    class Scrambles {
+        constructor(list) {
+            this.list = list
+            // list must be an array of strings
+            if (!this.list?.every || !this.list.every(x => typeof x == 'string')) {
+                throw "Scramble list must be an array of strings"
+            }
+        }
+
+        filter(f) {
+            return new Scrambles(this.list.filter(f))
+        }
+        
+        map(f) {
+            return new Scrambles(this.list.map(f))
+        }
+
+        flat() {
+            return new Scrambles(this.list.flat())
+        }
+
+        flatMap(f) {
+            return new Scrambles(this.list.flatMap(f))
+        }
+
+        // Takes strings/regexes or lists of strings/regexes
+        match(...patterns) {
+            // Flatten list of lists
+            patterns = patterns.flat()
+            const f = w => patterns.every(p => w.match(p))
+            return this.filter(f)
+        }
+        // Grammatical alias
+        thatMatch(...patterns) { return this.match(...patterns) }
+
+        // Takes a strings/regexes or lists of strings/regexes
+        notmatch(...patterns) {
+            // Flatten list of lists
+            patterns = patterns.flat()
+            const f = w => patterns.every(p => !w.match(p))
+            return this.filter(f)
+        }
+        // Grammatical alias
+        thatDontMatch(...patterns) { return this.notmatch(...patterns) }
+
+        isNotImpossible() {
+            return this.filter(isNotImpossible)
+        }
+        // Not not impossible === possible
+        isPossible() {
+            return this.filter(isNotImpossible)
+        }
+    }
+
+    const getLetters = () =>
+        Array.from(document.querySelectorAll(`.Key-module_key__kchQI`))
+            .filter(k => !k.ariaLabel?.match("absent"))
+            .map(k => k.innerText)
+            .filter(s => s.length == 1)
+
+    const getVowels = () => getLetters().filter(l => "AEIOUY".match(l))
+
+    // Wordle //
 
     unsafeWindow.solve = (wordWithUnderscores, without) => {
         if (wordWithUnderscores == undefined) {
@@ -45,11 +103,11 @@
         let words = [wordWithUnderscores]
         // Vowels
         while (words[0].match("@")) {
-               words = words.map(w => vowels.map(l => w.replace("@", l))).flat()
+               words = words.flatMap(w => vowels.map(l => w.replace("@", l)))
         }
         // Letters
         while (words[0].match("_")) {
-               words = words.map(w => letters.map(l => w.replace("_", l))).flat()
+               words = words.flatMap(w => letters.map(l => w.replace("_", l)))
         }
         // Q needs a U
         words = words.filter(w => !(w.match("Q[^U]")))
@@ -57,8 +115,8 @@
         words = words.filter(w => w.match(/[AEIOUY]/))
         // No triple letters
         words = words.filter(w => !w.match(/(\w)\1\1/))
-        // Add function to filter out impossible words
-        words.isNotImpossible = () => words.filter(isNotImpossible)
+
+        words = new Scrambles(words)
         return words
     }
     unsafeWindow.getLetters = getLetters
@@ -72,7 +130,7 @@
             const getAlpha = ls => [...ls].filter(k => k.innerText.match(/^\w$/) && !k.classList.contains("c-hahkls-idbAaef-css")).map(k => k.innerText)
             const letters = getAlpha($$(".c-hahkls"))
             const replaceFromW = (s, w) => w.map(a => s.replace("_", a))
-            const replaceNext = a => a.map(s => replaceFromW(s, letters)).flat()
+            const replaceNext = a => a.flatMap(s => replaceFromW(s, letters))
             // Starting word from green
             const css_selector_known_letters = ".c-hLErVa-ijlIsW-focused-true .c-eOfRZm:nth-of-type(2) .c-EbAgF.c-cyjHTc"
             let words = [$$(css_selector_known_letters).map(k => k.innerText ? k.innerText : "_").join("")]
@@ -94,8 +152,8 @@
                 // No triple letters
                 !w.match(/(\w)\1\1/)
             )
-            // Add function to filter out impossible words
-            words.isNotImpossible = () => words.filter(isNotImpossible)
+
+            words = new Scrambles(words)
             return words
         }
     }
